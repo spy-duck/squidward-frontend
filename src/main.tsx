df60b32d3907d94aec0d20db6f10292aef46d90e
Аlexander Kiselev <alexander.kiselev@mail.ru>
2025-12-10 18:33:34 +0300

    Added node proxy settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
-
+import consola from 'consola/browser';
 import { App } from './app'
 
+consola.info('version', import.meta.env.VITE_VERSION)
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(<App />)
