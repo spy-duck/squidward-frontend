0b4ca4118f72af9d79319c74c403a6e22ffa6d98
Аlexander Kiselev <alexander.kiselev@mail.ru>
2025-12-19 17:00:25 +0300

    - Added user metrics to list

--- src/features/users/users-list/users-list.tsx
+++ src/features/users/users-list/users-list.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { UsersListContract } from '@squidward/contracts/commands';
-import { Badge, Center, Paper, Table } from '@mantine/core';
-import { formatDateTime } from '@/shared/utils';
+import { Badge, Center, Paper, Popover, Text, Table } from '@mantine/core';
+import { bytesToGb, formatDateTime } from '@/shared/utils';
 import { UsersListItemMenu } from '@/features/users/users-list/users-list-item-menu';
 import { motion } from 'framer-motion';
 import { USER_STATUS_COLOR } from '@/shared/constants/users/user-status';
@@ -11,6 +11,7 @@ type UsersListProps = {
 };
 
 export function UsersList({ users }: UsersListProps): React.ReactElement {
+    console.log(users)
     const rows = (users || []).map((user, index) => (
         <motion.tr
             key={ user.uuid }
@@ -39,6 +40,62 @@ export function UsersList({ users }: UsersListProps): React.ReactElement {
             </Table.Td>
             <Table.Td>{ user.email }</Table.Td>
             <Table.Td>{ user.telegramId }</Table.Td>
+            <Table.Td>
+                <Popover width={ 200 } position='bottom' withArrow shadow='md'>
+                    <Popover.Target>
+                        <Badge
+                            variant='transparent'
+                            className='cursor-pointer'
+                            color={ user.metrics?.total ? 'teal' : 'gray' }
+                        >
+                            { bytesToGb(user.metrics?.total || 0) } Gib
+                        </Badge>
+                    </Popover.Target>
+                    <Popover.Dropdown>
+                        
+                        <Table>
+                            <Table.Tbody>
+                                <Table.Tr>
+                                    <Table.Td>
+                                        <Text size='xs'>
+                                            Upload:
+                                        </Text>
+                                    </Table.Td>
+                                    <Table.Td>
+                                        <Text size='xs'>
+                                            { bytesToGb(user.metrics?.upload || 0) } Gib
+                                        </Text>
+                                    </Table.Td>
+                                </Table.Tr>
+                                <Table.Tr>
+                                    <Table.Td>
+                                        <Text size='xs'>
+                                            Download:
+                                        </Text>
+                                    </Table.Td>
+                                    <Table.Td>
+                                        <Text size='xs'>
+                                            { bytesToGb(user.metrics?.download || 0) } Gib
+                                        </Text>
+                                    </Table.Td>
+                                </Table.Tr>
+                                <Table.Tr>
+                                    <Table.Td>
+                                        <Text size='xs'>
+                                            Total:
+                                        </Text>
+                                    </Table.Td>
+                                    <Table.Td>
+                                        <Text size='xs'>
+                                            { bytesToGb(user.metrics?.total || 0) } Gib
+                                        </Text>
+                                    </Table.Td>
+                                </Table.Tr>
+                            </Table.Tbody>
+                        </Table>
+                    </Popover.Dropdown>
+                </Popover>
+            </Table.Td>
             <Table.Td>{ formatDateTime(user.createdAt) }</Table.Td>
             <Table.Td>{ formatDateTime(user.expireAt) }</Table.Td>
         </motion.tr>
@@ -52,7 +109,8 @@ export function UsersList({ users }: UsersListProps): React.ReactElement {
                         <Table.Th>Name</Table.Th>
                         <Table.Th>status</Table.Th>
                         <Table.Th>Email</Table.Th>
-                        <Table.Th>telegramId</Table.Th>
+                        <Table.Th>Telegram ID</Table.Th>
+                        <Table.Th>Traffic</Table.Th>
                         <Table.Th style={ { width: 160 } }>Created at</Table.Th>
                         <Table.Th style={ { width: 160 } }>Expire at</Table.Th>
                     </Table.Tr>
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './format-datetime';
+export * from './format-traffic';
